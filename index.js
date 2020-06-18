bd16824534ede2a132842ac990fa2aec498a729d
袁柱 <yzsylinder@163.com>
2020-06-18 13:16:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
+console.log(name.toUpperCase());
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+function capitalize(str) {
+    var array = str.toLowerCase().split(" ");
+    for (var i = 0; i < array.length; i++) {
+        array[i] = array[i][0].toUpperCase() + array[i].substring(1, array[i].length);
+    }
+    var result = array.join(" ");
+    return result;
+}
+console.log(sentence);
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+var num= s.replace(/[^0-9]/ig,"");
+console.log(num);
